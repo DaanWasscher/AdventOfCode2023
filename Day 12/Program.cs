13482edbbb9faabb3db29f58e8ad004cfd33c643
Daan Wasscher <ext.daan.wasscher@sdworx.com>
2023-12-21 15:23:21 +0100

    Day 12 part 1

---
+++ Day 12/Program.cs
@@ -0,0 +1,26 @@
+﻿using Day_12;
+
+var inputs = InputParser.GetSpringArrangements();
+var possibleArrangements = 0;
+
+foreach (var input in inputs)
+{
+    possibleArrangements+= input.GetNumberOfPossibleArrangements();
+}
+
+
+Console.WriteLine($"There are {possibleArrangements} possible arrangements in part 1");
+
+
+//part 2
+
+var inputs2 = InputParser.GetSpringArrangementsPart2();
+long possibleArrangementsPart2 = 0;
+
+foreach (var input in inputs2)
+{
+    possibleArrangements += input.GetNumberOfPossibleArrangementsPart2();
+}
+
+
+Console.WriteLine($"There are {possibleArrangementsPart2} possible arrangements in part 2");
